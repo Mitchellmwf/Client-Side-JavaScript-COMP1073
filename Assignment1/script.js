6cdc79f5643869bf2ddbd7b3617e849d5f4d1fcf
Mitchellmwf <mitchellmwf@gmail.com>
2026-06-17 17:03:53 -0400

    Index buttons and styling. JS button fetching

---
+++ Assignment1/script.js
@@ -0,0 +1,6 @@
+const subBtn = document.getElementById('subject');
+const verbBtn = document.getElementById('verb');
+const adjBtn = document.getElementById('adjective');
+const objBtn = document.getElementById('object');
+const locBtn = document.getElementById('location');
+
